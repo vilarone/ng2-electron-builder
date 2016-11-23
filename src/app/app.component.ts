07829f9e8b44e587997d2f8fa4cdb56d7a6d66a5
vilarone <vilarone@gmail.com>
2016-11-23 15:10:59 +0100

    first test of events between main and renderer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronEventService } from './event.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  public lang: string;
+  constructor(){
+    ElectronEventService.on('changeSample').subscribe((e: any) => {
+        this.changeSample({ target: { value: e.detail.value } });
+      });
+  }
+  changeSample(e: any) {
+    let lang: string = "none";
+    if (e && e.target) {
+      lang = e.target.value;
+    }
+    console.log(`Language change: ${lang}`);
+    this.lang =lang;
+  }
 }
