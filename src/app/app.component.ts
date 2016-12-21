acd85f22b216db9b43cac78ef0cfe30df57a5737
David Vilaró <vilarone@gmail.com>
2016-12-21 12:01:00 +0100

    electron into angular and added some examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ElectronEventService } from './event.service';
-
+import { remote } from 'electron';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +8,34 @@ import { ElectronEventService } from './event.service';
 })
 export class AppComponent {
   title = 'app works!';
+  errorbox = { title: 'sample title', content: 'samplecontent'};
+  typebox = ['none', 'info', 'error', 'question', 'warning'];
+  // typebox = [{value: 'none', label: 'none'}, {value: 'info', label: 'info'}, {value: 'error', label: 'error'}, {value: 'question', label: 'question'}, {value: 'warning', label: 'warning'}];
+  optionsmbox = {
+    type: 'info',
+    buttons: ['Custom Button', 'Cancel', 'OK'],
+    title: 'Title',
+    message: 'Message'
+  };
   public lang: string;
   constructor(){
     ElectronEventService.on('changeSample').subscribe((e: any) => {
         this.changeSample({ target: { value: e.detail.value } });
       });
   }
+  openErrorBox(title, content) {
+    remote.dialog.showErrorBox(title, content);
+  }
+  openMessageBox(options) {
+    remote.dialog.showMessageBox(options);
+  }
+  openOpenDialog(){
+    remote.dialog.showOpenDialog([]);
+  }
+
   changeSample(e: any) {
-    let lang: string = "none";
+    // tslint:disable-next-line:no-inferrable-types
+    let lang: string = 'none';
     if (e && e.target) {
       lang = e.target.value;
     }
